83aec2fad6ea59f054aca47b045648be92f0f2f2
douglasUtzig <52766902+douglasUtzig@users.noreply.github.com>
2024-03-07 15:03:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-//comentário para fazer um commit 
-
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
