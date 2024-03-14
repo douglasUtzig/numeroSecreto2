89c2fa6c31afdc6969e508f44ff162e9cbb02784
Douglas Utzig <douglasutzig.1@gmai.com>
2024-03-14 17:13:59 -0300

    Adicionando comentário com mais de uma linha

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ function exibirTextoNaTela(tag, texto) {
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
+/*
+Novo comentario
+*/
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
